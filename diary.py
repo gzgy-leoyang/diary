93c949ca51e78cfae05c6d0cf10c6a947e62e990
gzgy-leoyang <leoyang20102013@163.com>
2020-02-06 21:46:54 +0800

    修改配置文件，添加本地日志文件名称设置，而且本地文件名也同步为远程文件名，这样在服务器端就可以区分多个用户

--- diary.py
+++ diary.py
@@ -11,61 +11,59 @@ import time
 from datetime import date,datetime,time
 
 import configparser
-
-#################################################33
-# 日记文件名
-diary_file = "log.xlsx"
-
-# 包含路径的日记文件名
-file_name = None
-
-# 全局，当前路径
-cur_path = None
-
-auto_upload_int = None
-server_ip_str = None
-server_port_str = None
-server_user_str = None
-server_pass_str = None
+import re
 
 ############################
 # @berif 打印 help 信息
 def usage( ):
-    print(' Usage: python3 diary.py commit ')
     print(' Diary v1.0.0  2020/2/3 ( leoyang20102013@163.com )')
 
+############
+## ini 文件操作
 def construct_default_config():
+    server_ip_str = input(" 服务器IP : ")
+
+    if not re.match(  r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$' ,server_ip_str):
+        print ( "IP address invaild ")
+        exit()
+
+    server_port_str = input(" 服务器Port : ")
+    if not re.match(  r'^(?:[0-9]{1,5})$' ,server_port_str):
+        print ( "Port invaild ")
+        exit()
+
+    server_user_str = input(" 用户名: ")
+    server_pass_str = input(" 用户密码: ")
+    local_file_name_str = input(" 日志文件名: ")
 
     config = configparser.ConfigParser()
     # 默认，每周第一天第一次记录时，上传日志
-    config["DEFAULT"] = { "auto_upload":"1"}
+    config["DEFAULT"] = { "auto_upload":"1" , "local_file_name":local_file_name_str  }
     auto_upload_int = 1
-    server_ip_str = input(" 服务器IP (xxx.xxx.xxx.xxx): ")
-    server_port_str = input(" 服务器Port (0~65535): ")
-    server_user_str = input(" 用户名: ")
-    server_pass_str = input(" 用户密码: ")
+    local_file_name = local_file_name_str
 
     config["FTP Server"] = {}
     config["FTP Server"]["serverIP"] = server_ip_str
     config["FTP Server"]["serverPort"] = server_port_str
     config["FTP Server"]["userName"] = server_user_str
     config["FTP Server"]["password"] = server_pass_str
-
-    with open("config.ini","w") as config_file :
-        config.write(config_file)
+    with open(  sys.path[0] + '/config.ini' ,"w") as fd :
+        config.write( fd )
     
-    return auto_upload_int,server_ip_str,server_port_str,server_user_str,server_pass_str
+    return auto_upload_int,  local_file_name , server_ip_str,server_port_str,server_user_str,server_pass_str
 
 def parser_config():
-    cfg_name  = cur_path+'/config.ini'
+    cfg_name  = sys.path[0] + '/config.ini'
     if not os.access( cfg_name , os.F_OK ):
-        print (" 首次使用需要配置服务器参数：")
+        print (" 首次使用,配置服务器参数：")
         return construct_default_config()
     else :
         cfg_file = configparser.ConfigParser()
         cfg_file.read( cfg_name)
+
         if "DEFAULT" in cfg_file :
             auto_upload_int = cfg_file["DEFAULT"].getint("auto_upload")
+            local_file_name  = cfg_file["DEFAULT"]["local_file_name"]
         else :
             auto_upload_int = 1
 
@@ -78,26 +76,9 @@ def parser_config():
             server_ip_str = "192.168.1.105"
             server_port_str = "21"
             server_user_str = "user"
-            server_pass_str = "111"
-    return auto_upload_int,server_ip_str,server_port_str,server_user_str,server_pass_str
+            server_pass_str = "123"
+    return auto_upload_int , local_file_name , server_ip_str,server_port_str,server_user_str,server_pass_str
 
-
-###################
-# @ berif
-#
-def parser_content( argv ):
-    if argv[1] == "commit" :
-        # 返回记录的内容
-        return input(" [随手记 ]")  
-    elif argv[1] == "push":
-        # ftp_service()
-        exit()
-    elif (argv[1] == "help") :
-        usage( )
-        exit()
-    else:
-        exit()
-    
 def get_week_date():
     datetime_obj = datetime.now()
     cur_date_str = datetime_obj.date().strftime("%Y-%m-%d")
@@ -110,17 +91,12 @@ def get_week_date():
     return cur_date_str,cur_time_str,cur_week_int,cur_weekDay_int
 
 def get_workbook( fileName ):
-    # ## 查询是否有文件，如果有该文件，执行删除，再重新建同名文件  
-    global file_name
-    # cur_path  = sys.path[0]
-    # file_name  = cur_path+'/'+fileName
-    if not os.access( file_name , os.F_OK ):
-        print (" FileNotFoundError " , reason )
+    if not os.access( fileName , os.F_OK ):
+        print (" File Not Found Error ")
         exit()
-    return openpyxl.load_workbook( file_name )
+    return openpyxl.load_workbook( fileName )
 
-def get_sheet( wb_obj , title_str ):
-    global file_name
+def get_sheet( file,wb_obj , title_str ):
     try:
         sheet_list = wb_obj.get_sheet_names()
         sheet_index = sheet_list.index( title_str )
@@ -128,74 +104,61 @@ def get_sheet( wb_obj , title_str ):
         temp_sheet_index = sheet_list.index( "temp" )
         sheet_obj = wb_obj.copy_worksheet ( wb_obj.worksheets[ temp_sheet_index ]  )
         sheet_obj.title = title_str
-        wb_obj.save( file_name )
+        wb_obj.save( file )
     else :
         sheet_obj = wb_obj.worksheets[ sheet_index ]
     finally:
         return sheet_obj
 
-def ftp_service( ip, port, userName, password ):
-    remote_file_path = diary_file
+def ftp_upload( remote_file , local_file , ip,  userName, password ):
     ftp = FTP(host=ip , user=userName , passwd=password)
     ftp.login( user=userName , passwd=password)
-
-    # 用户名登陆，OK
-    # ftp = FTP(host="192.168.1.105",user="user",passwd="123")
-    # ftp.login( user="user",passwd="123" )
-
-    # 匿名登陆，文件写入 ftp-anonymous/ 下，OK
+    # 匿名登陆，文件写入 ftp-anonymous/ 
     # ftp = FTP(host="192.168.1.105")
-    # ftp.set_debuglevel(2)
     # ftp.login()
-
-    with open( file_name,"rb") as local_file :
-        ftp.storbinary('STOR ' + remote_file_path ,local_file ,1024 )
+    with open( local_file,"rb") as fd :
+        ftp.storbinary('STOR ' + remote_file , fd ,1024 )
     ftp.quit()
 
 #################
 def main():
-    global file_name
-    global cur_path
-
-    cur_path  = sys.path[0]
-    file_name  = cur_path+'/'+diary_file
-
-    # 获取当前时间，包括年周数（一年内的第几周）和周日数（一周内的第几天）
     date_str,time_str,week_int,weekDay_int = get_week_date()
-    # 根据年周数，确定表格名称
     sheet_title_str = str( week_int )
 
-    # 提取命令行参数中的日志写入内容
-    content_str = parser_content( sys.argv )
+    auto_upload_int, local_file_name, server_ip_str,server_port_str,server_user_str,server_pass_str = parser_config() 
+    local_file_path  = sys.path[0] +'/' + local_file_name
 
-    ## 获取服务器配置
-    auto_upload_int,server_ip_str,server_port_str,server_user_str,server_pass_str = parser_config() 
+    if sys.argv[1] == "commit" :
+        content_str = input(" [随手记 ]")
+        if content_str != "":
+            wb = get_workbook(  local_file_path )
+            sheet = get_sheet( local_file_path , wb , sheet_title_str )
 
+            privous_str = sheet.cell( weekDay_int+1 ,2).value
+            sheet.cell( weekDay_int+1 ,2).alignment = Alignment( horizontal="left",vertical="top")
+            
+            if privous_str == None :
+                # 空白cell
+                sheet.cell( weekDay_int+1 ,2).value = "[ "+ time_str+"] "+ content_str
+            else :
+                sheet.cell( weekDay_int+1 ,2).value = privous_str +"\n[ "+ time_str+"]  "+ content_str
+            wb.save( local_file_path )
+            
+            if  auto_upload_int == weekDay_int  :
+                ftp_upload( local_file_name , local_file_path ,server_ip_str, server_user_str, server_pass_str )
+        else :
+            print (" 输入内容为空，不写入任何内容")
+            exit()
+    elif sys.argv[1] == "push":
+        ftp_upload( local_file_name , local_file_path ,server_ip_str, server_user_str, server_pass_str )
+        exit()
+    elif sys.argv[1] == "help" :
+        usage( )
+        exit()
+    else:
+        exit()
 
-    # 获取工作簿及其中的对应表
-    wb = get_workbook(  diary_file )
-    sheet = get_sheet( wb , sheet_title_str )
-    
-    # 添加内容 
-    privous_str = sheet.cell( weekDay_int+1 ,2).value
-    # 检查是否是新cell，没有内容时，不追加新的内容
-    if privous_str == None :
-        sheet.cell( weekDay_int+1 ,2).value = "[ "+ time_str+"]"+ content_str
-    else :
-        sheet.cell( weekDay_int+1 ,2).value = privous_str +"\n[ "+ time_str+"]"+ content_str
-    
-    # 设置对齐格式
-    sheet.cell( weekDay_int+1 ,2).alignment = Alignment( horizontal="left",vertical="top")
-    wb.save(file_name)
-
-    ## 检查自动上传的条件，在这一天之内，每次记录都会触发一次上传
-    ## TODO：这个部分需要再优化 
-    if  auto_upload_int == weekDay_int  :
-        print ( " auto_upload_int == weekDay_int ,%s,%s,%s,%s" % ( server_ip_str, server_port_str, server_user_str, server_pass_str ) )
-        ftp_service( server_ip_str, server_port_str, server_user_str, server_pass_str )
     return 
 
-#################
-## <<  程序入口 >> ##
 if __name__ == "__main__":
     main()
