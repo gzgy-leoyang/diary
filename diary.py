5e0c3507803fd7e5bd985293e0457b13cdf78b3c
gzgy-leoyang <leoyang20102013@163.com>
2020-02-22 17:17:43 +0800

    feat:增加运行环境的检查，包括配置文件和记录文件的检查

--- diary.py
+++ diary.py
@@ -14,56 +14,10 @@ from openpyxl.styles import Alignment
 import configparser
 from datetime import date,datetime,time
 
-############################
-# @berif 打印 help 信息
-def usage( ):
-    print(' Usage: diary <cmd> [opt]')
-    print(' cmd:')
-    print('     commit  ：提交一条记录，Enter 结束输入')
-    print('     show  [ week ] : 显示第 week 周的内容')
-    print('     push    : 向 FTP 服务器推送本地记录文件')
-    print(' Diary v1.0.0  2020/2/3 ( leoyang20102013@163.com )')
 
 ############
 ## ini 文件操作
 ## TODO 构造配置文件，只返回是否成功
-def construct_default_config( config_file_name):
-    server_ip_str = input(" 服务器IP : ")
-
-    if not re.match(  r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$' ,server_ip_str):
-        print ( "IP address invaild ")
-        exit()
-
-    server_port_str = input(" 服务器Port : ")
-    if not re.match(  r'^(?:[0-9]{1,5})$' ,server_port_str):
-        print ( "Port invaild ")
-        exit()
-
-    server_online_str = input(" 网络支持 (y/n): ")
-    if server_online_str != "y" :
-        server_online_str = "n"
-
-    server_user_str = input(" 用户名: ")
-    server_pass_str = input(" 用户密码: ")
-    local_file_name_str = input(" 日志文件名: ")
-
-    config = configparser.ConfigParser()
-    # 默认，每周第一天第一次记录时，上传日志
-    config["DEFAULT"] = { "auto_upload":"1" , "local_file_name":local_file_name_str  }
-    auto_upload_int = 1
-    local_file_name = local_file_name_str
-
-    config["FTP Server"] = {}
-    config["FTP Server"]["online"] = server_online_str
-    config["FTP Server"]["serverIP"] = server_ip_str
-    config["FTP Server"]["serverPort"] = server_port_str
-    config["FTP Server"]["userName"] = server_user_str
-    config["FTP Server"]["password"] = server_pass_str
-
-    ## 异常处理，写异常需要处理一下
-    with open(  sys.path[0] + '/'+config_file_name ,"w") as fd :
-        config.write( fd )  
-    return auto_upload_int,  local_file_name , server_ip_str,server_port_str,server_online_str,server_user_str,server_pass_str
 
 ## 测试：test_parser_config()
 ## TODO 将配置文件的解析，分为独立部分，目前的方式缺乏灵活性，也不利于作测试
@@ -104,6 +58,17 @@ def parser_config( config_file_name="config.ini" ):
             server_pass_str = "123"
     return auto_upload_int , local_file_name , server_ip_str,server_port_str,server_online_str,server_user_str,server_pass_str
 
+
+    config_file_path  = sys.path[0] +'/'+config_file_name
+    cfg_file = configparser.ConfigParser()
+    cfg_file.read( config_file_path)
+    if "DEFAULT" in cfg_file :
+        local_file_name  = cfg_file["DEFAULT"]["local_file_name"]
+    else :
+        local_file_name="local_diary.xlsx"
+    return local_file_name
+
+
 def get_auto_upload( config_file_name="config.ini" ):
     if config_file_name == "":
         print ("配置文件名为空")
@@ -126,20 +91,7 @@ def get_auto_upload( config_file_name="config.ini" ):
             auto_upload_int = 1
     return auto_upload_int
 
-## TODO:
-# 添加工作环境检查，启动时首先检查一下记录文件和配置文件是否存在，如果
-# 如果不存在，则判断为首次运行，生成这两个文件
-#  *配置文件中，记录文件名被返回，用于建立记录文件实例
-def check_env( ):
-    config_file_path  = sys.path[0] +'/config.ini'
-    if not os.access( config_file_path , os.F_OK ):
-        print (" 首次使用,配置服务器参数：")
-        construct_default_config( config_file_name )
-    
-    record_file_path  = sys.path[0] +'/'+record_file_name
-    if not os.access( record_file_path , os.F_OK ):
-        print (" 首次使用,配置服务器参数：")
-        construct_default_config( config_file_name )
+
 
 
 def get_date():
@@ -157,21 +109,20 @@ def get_time():
     return datetime.now().time().strftime("%H:%M:%S")
 
 
-## 测试: test_diary.test_get_workbook()
-def get_workbook( fileName = "yangj_log.xlsx" ):
-    if fileName == "":
-        return None
-    else :
-        if not fileName.endswith(".xlsx")  :
-            return None
 
     if not os.access( fileName , os.F_OK ):
+        # 新建文件及 wb，#0 sheet，向其中填充模板
         wb = openpyxl.Workbook()
-        sheet = wb .create_sheet("temp")
+        sheet = wb .create_sheet("0") 
+        day_in_week_str = ["周一","周二","周三","周四","周五","周六","周日"]
+        sheet.cell( 1 ,1).value = "日期"
+        sheet.cell( 1 ,2).value = "内容"
+        for i in range(0,7):
+            print ( i,day_in_week_str[i] )
+            sheet.cell( i+2 ,1).value = day_in_week_str[i]
         wb.save(fileName)
         print (" File not found ,then create new workbook")
         return wb
-    print ( "%s" % fileName )
     return openpyxl.load_workbook( fileName )
 
 def get_sheet( file,wb_obj , title_str ):
@@ -235,21 +186,94 @@ def commit( file,content_str ):
     else :
         print (" 输入内容为空，不写入任何内容")
 
+
+## 测试: test_diary.test_get_workbook()
+def get_workbook( fileName = "yangj_log.xlsx" ):
+    if fileName == "":
+        return None
+    else :
+        if not fileName.endswith(".xlsx")  :
+            return None
+
+def get_reocrd_file_name( config_file_name="config.ini" ):
+    if config_file_name == "":
+        return None
+    else :
+        if not config_file_name.endswith(".ini") :
+            return None
+
+## 创建配置文件
+def construct_default_config( config_file_path="config.ini"):
+    config = configparser.ConfigParser()
+    config["FTP Server"] = {}
+
+    server_ip_str = input(" 服务器IP : ")
+    if not re.match(  r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$' ,server_ip_str):
+        print ( "IP address invaild ")
+        exit()
+    config["FTP Server"]["serverIP"] = server_ip_str
+
+    server_port_str = input(" 服务器Port : ")
+    if not re.match(  r'^(?:[0-9]{1,5})$' ,server_port_str):
+        print ( "Port invaild ")
+        exit()
+    config["FTP Server"]["serverPort"] = server_port_str
+
+    server_online_str = input(" 网络支持 (y/n): ")
+    if server_online_str != "y" :
+        server_online_str = "n"
+    config["FTP Server"]["online"] = server_online_str
+
+    server_user_str = input(" 用户名: ")
+    config["FTP Server"]["userName"] = server_user_str
+
+    server_pass_str = input(" 用户密码: ")
+    config["FTP Server"]["password"] = server_pass_str
+
+    config["DEFAULT"] = {}
+    local_file_name_str = input(" 日志文件(*.xlsx): ")
+    config["DEFAULT"]["local_file_name"] = local_file_name_str
+    config["DEFAULT"]["auto_upload"] = "1" ## 默认1，未来可能取消自动推送功能，改用 push 命令推送
+
+    #TODO:写异常需要处理
+    with open(  config_file_path ,"w") as fd :
+        config.write( fd ) 
+    return 0
+
+def check_environment( ):
+    config_file_path  = sys.path[0] +'/config.ini'
+    if not os.access( config_file_path , os.F_OK ):
+        print (" 首次使用,配置服务器参数：")
+        construct_default_config( config_file_path)
+    else :
+        record_file_name = get_reocrd_file_name()
+    record_file_path  = sys.path[0] +'/'+record_file_name
+    if not os.access( record_file_path , os.F_OK ):
+        get_workbook(  record_file_name )
+    return 0
+
+############################
+# @berif 打印 help 信息
+def usage( ):
+    print(' Usage: diary <cmd> [opt]')
+    print(' cmd:')
+    print('     commit  ：提交一条记录，Enter 结束输入')
+    print('     show  [ week ] : 显示第 week 周的内容')
+    print('     push    : 向 FTP 服务器推送本地记录文件')
+    print(' Diary v0.0.1  2020/2/3 ( leoyang20102013@163.com )')
+
 #################
 def main():
     if sys.argv.__len__() <= 1:
         usage()
         exit()
-    
-    auto_upload_int, local_file_name, server_ip_str,server_port_str,server_online_str,server_user_str,server_pass_str = parser_config("config.ini") 
-    local_file_path  = sys.path[0] +'/' + local_file_name
 
+    if check_environment() < 0 :
+        exit()
+
+    local_file_path  = sys.path[0] +'/' + get_reocrd_file_name( )
     if sys.argv[1] == "commit" :
         commit( local_file_path,input("[ 随手记 ]")  )
-        # if  (auto_upload_int == weekDay_int) and (server_online_str == "y")  :
-        #     ret = ftp_upload( local_file_name , local_file_path ,server_ip_str, server_user_str, server_pass_str )
-        #     if ret == None :
-        #         print ("确认服务器IP配置正确或服务器已经启动")
         exit()
     elif sys.argv[1] == "show" :
         week_int = get_week()
